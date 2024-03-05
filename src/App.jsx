36b8dd072803bda95f0567b547d07a26fb5f6525
unknown <todorovicslobodan10@gmail.com>
2024-03-05 16:23:19 +0000

    Added component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
 
 function App() {
-  return <>Portfolio</>;
+  return (
+    <>
+      Portfolio
+      <Navbar></Navbar>
+      <Header></Header>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
