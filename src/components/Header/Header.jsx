15fa7122e2839762a936ea9458ccfad16a54604a
unknown <todorovicslobodan10@gmail.com>
2024-04-05 15:59:00 +0100

    added some boilerplate for the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
+// Bootstrap imports
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+// styles import
+import "./Header.css";
+
 const Header = () => {
   return (
     <>
       <Container>
         <Row>
-          <Col>
+          <Col className="pt-5" sm={{ order: "first" }}>
             <h3> Hi, I am a Software Developer </h3>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -15,11 +21,13 @@ const Header = () => {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
+            <Button variant="danger">Hire me</Button>
           </Col>
-          <Col>
+          <Col xs={{ order: "first" }}>
             <Image
-              src="../src/assets/images/avataaars.svg"
+              src="../src/assets/images/avatar.png"
               alt="Profile Avatar"
+              id="design"
             />
           </Col>
         </Row>
